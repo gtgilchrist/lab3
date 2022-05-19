512a5e959994d38c9988f0c38fe7c2516973ed93
Grant Gilchrist <gilchrist@ucla.edu>
2022-05-18 19:06:53 -0700

    Adding v2

--- lab3/hash-table-v2.c
+++ lab3/hash-table-v2.c
@@ -15,6 +15,7 @@ SLIST_HEAD(list_head, list_entry);
 
 struct hash_table_entry {
 	struct list_head list_head;
+	pthread_mutex_t lock;
 };
 
 struct hash_table_v2 {
@@ -27,6 +28,10 @@ struct hash_table_v2 *hash_table_v2_create()
 	assert(hash_table != NULL);
 	for (size_t i = 0; i < HASH_TABLE_CAPACITY; ++i) {
 		struct hash_table_entry *entry = &hash_table->entries[i];
+		int status = pthread_mutex_init(&entry->lock,NULL);
+		if(status != 0){
+			exit(status);
+		}
 		SLIST_INIT(&entry->list_head);
 	}
 	return hash_table;
@@ -71,6 +76,10 @@ void hash_table_v2_add_entry(struct hash_table_v2 *hash_table,
                              uint32_t value)
 {
 	struct hash_table_entry *hash_table_entry = get_hash_table_entry(hash_table, key);
+	int status = pthread_mutex_lock(&hash_table_entry->lock);
+	if(status != 0){
+		exit(status);
+	}
 	struct list_head *list_head = &hash_table_entry->list_head;
 	struct list_entry *list_entry = get_list_entry(hash_table, key, list_head);
 
@@ -84,6 +93,10 @@ void hash_table_v2_add_entry(struct hash_table_v2 *hash_table,
 	list_entry->key = key;
 	list_entry->value = value;
 	SLIST_INSERT_HEAD(list_head, list_entry, pointers);
+	status = pthread_mutex_unlock(&hash_table_entry->lock);
+	if(status != 0){
+		exit(status);
+	}
 }
 
 uint32_t hash_table_v2_get_value(struct hash_table_v2 *hash_table,
@@ -100,6 +113,10 @@ void hash_table_v2_destroy(struct hash_table_v2 *hash_table)
 {
 	for (size_t i = 0; i < HASH_TABLE_CAPACITY; ++i) {
 		struct hash_table_entry *entry = &hash_table->entries[i];
+		int status = pthread_mutex_destroy(&entry->lock);
+		if(status != 0){
+			exit(status);
+		}
 		struct list_head *list_head = &entry->list_head;
 		struct list_entry *list_entry = NULL;
 		while (!SLIST_EMPTY(list_head)) {
