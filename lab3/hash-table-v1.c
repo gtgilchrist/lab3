85b90c1fcd72011f0ee0083543ef2104b8d2af9d
Grant Gilchrist <gilchrist@ucla.edu>
2022-05-19 15:41:35 -0700

    changed v1 to functions for locks

--- lab3/hash-table-v1.c
+++ lab3/hash-table-v1.c
@@ -24,8 +24,30 @@ struct hash_table_entry {
 struct hash_table_v1 {
 	struct hash_table_entry entries[HASH_TABLE_CAPACITY];
 };
+
 void init_lock(void){
 	int status = pthread_mutex_init(&lock,NULL);
+	if(status != 0){
+		exit(status);
+	}
+}
+
+void lock_lock(void){
+	int status = pthread_mutex_lock(&lock);
+	if(status != 0){
+		exit(status);
+	}
+}
+
+void unlock_lock(void){
+	int status = pthread_mutex_unlock(&lock);
+	if(status != 0){
+		exit(status);
+	}
+}
+
+void destroy_lock(void){
+	int status = pthread_mutex_destroy(&lock);
 	if(status != 0){
 		exit(status);
 	}
@@ -81,10 +103,7 @@ void hash_table_v1_add_entry(struct hash_table_v1 *hash_table,
                              const char *key,
                              uint32_t value)
 {
-	int status = pthread_mutex_lock(&lock);
-	if(status != 0){
-		exit(status);
-	}
+	lock_lock();
 	struct hash_table_entry *hash_table_entry = get_hash_table_entry(hash_table, key);
 	struct list_head *list_head = &hash_table_entry->list_head;
 	struct list_entry *list_entry = get_list_entry(hash_table, key, list_head);
@@ -99,10 +118,7 @@ void hash_table_v1_add_entry(struct hash_table_v1 *hash_table,
 	list_entry->key = key;
 	list_entry->value = value;
 	SLIST_INSERT_HEAD(list_head, list_entry, pointers);
-	status = pthread_mutex_unlock(&lock);
-	if(status != 0){
-		exit(status);
-	}
+	unlock_lock();
 }
 
 uint32_t hash_table_v1_get_value(struct hash_table_v1 *hash_table,
@@ -117,10 +133,7 @@ uint32_t hash_table_v1_get_value(struct hash_table_v1 *hash_table,
 
 void hash_table_v1_destroy(struct hash_table_v1 *hash_table)
 {
-	int status = pthread_mutex_destroy(&lock);
-	if(status != 0){
-		exit(status);
-	}
+	destroy_lock();
 	for (size_t i = 0; i < HASH_TABLE_CAPACITY; ++i) {
 		struct hash_table_entry *entry = &hash_table->entries[i];
 		struct list_head *list_head = &entry->list_head;
