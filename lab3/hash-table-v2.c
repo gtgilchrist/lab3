2f5445ddf48e3a348b59d346e681bc9f99018f01
Grant Gilchrist <gilchrist@ucla.edu>
2022-05-19 16:58:25 -0700

    test

--- lab3/hash-table-v2.c
+++ lab3/hash-table-v2.c
@@ -105,7 +105,6 @@ void hash_table_v2_add_entry(struct hash_table_v2 *hash_table,
 	lock_lock_v2(&hash_table_entry->lock);
 	struct list_head *list_head = &hash_table_entry->list_head;
 	struct list_entry *list_entry = get_list_entry(hash_table, key, list_head);
-	unlock_lock_v2(&hash_table_entry->lock);
 
 	/* Update the value if it already exists */
 	if (list_entry != NULL) {
@@ -116,8 +115,8 @@ void hash_table_v2_add_entry(struct hash_table_v2 *hash_table,
 	list_entry = calloc(1, sizeof(struct list_entry));
 	list_entry->key = key;
 	list_entry->value = value;
-
 	SLIST_INSERT_HEAD(list_head, list_entry, pointers);
+	unlock_lock_v2(&hash_table_entry->lock);
 
 
 }
