4d8d53d99e1a53e330c123e7fd1c482d982a86b9
Grant Gilchrist <gilchrist@ucla.edu>
2022-05-20 16:28:17 -0700

    test

--- lab3/hash-table-tester.c
+++ lab3/hash-table-tester.c
@@ -258,11 +258,11 @@ int main(int argc, char *argv[])
 	size_t higher = base_times /(arguments.threads / 2);
 	printf("%'lu < %'lu < %'lu\n",lower,v2_times,higher);
 	if(lower < v2_times)
-		printf("v2 fast?\n")
+		printf("v2 fast?\n");
 	else if(higher < v2_times)
-		printf("V2 slow\n")
+		printf("V2 slow\n");
 	else
-		printf("Just Right!\n")
+		printf("Just Right!\n");
 
 	return 0;
 }
