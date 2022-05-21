a1763645a79eeef8fe676b5206d54ada689b3bed
Grant Gilchrist <gilchrist@ucla.edu>
2022-05-20 18:37:31 -0700

    test

--- lab3/hash-table-v1.c
+++ lab3/hash-table-v1.c
@@ -7,7 +7,7 @@
 #include <string.h>
 #include <sys/queue.h>
 
-pthread_mutex_t lock;
+
 
 struct list_entry {
 	const char *key;
@@ -23,31 +23,32 @@ struct hash_table_entry {
 
 struct hash_table_v1 {
 	struct hash_table_entry entries[HASH_TABLE_CAPACITY];
+	pthread_mutex_t lock;
 };
 
-void init_lock_v1(void){
-	int status = pthread_mutex_init(&lock,NULL);
+void init_lock_v1(pthread_mutex_t *lock){
+	int status = pthread_mutex_init(lock,NULL);
 	if(status != 0){
 		exit(status);
 	}
 }
 
-void lock_lock_v1(void){
-	int status = pthread_mutex_lock(&lock);
+void lock_lock_v1(pthread_mutex_t *lock){
+	int status = pthread_mutex_lock(lock);
 	if(status != 0){
 		exit(status);
 	}
 }
 
-void unlock_lock_v1(void){
-	int status = pthread_mutex_unlock(&lock);
+void unlock_lock_v1(pthread_mutex_t *lock){
+	int status = pthread_mutex_unlock(lock);
 	if(status != 0){
 		exit(status);
 	}
 }
 
-void destroy_lock_v1(void){
-	int status = pthread_mutex_destroy(&lock);
+void destroy_lock_v1(pthread_mutex_t *lock){
+	int status = pthread_mutex_destroy(lock);
 	if(status != 0){
 		exit(status);
 	}
@@ -57,7 +58,7 @@ struct hash_table_v1 *hash_table_v1_create()
 {
 	struct hash_table_v1 *hash_table = calloc(1, sizeof(struct hash_table_v1));
 	assert(hash_table != NULL);
-	init_lock_v1();
+	init_lock_v1(&hash_table->lock);
 	for (size_t i = 0; i < HASH_TABLE_CAPACITY; ++i) {
 		struct hash_table_entry *entry = &hash_table->entries[i];
 		SLIST_INIT(&entry->list_head);
@@ -104,7 +105,7 @@ void hash_table_v1_add_entry(struct hash_table_v1 *hash_table,
                              uint32_t value)
 {
 	struct hash_table_entry *hash_table_entry = get_hash_table_entry(hash_table, key);
-	lock_lock_v1();
+	lock_lock_v1(&hash_table->lock);
 	struct list_head *list_head = &hash_table_entry->list_head;
 	struct list_entry *list_entry = get_list_entry(hash_table, key, list_head);
 
@@ -118,7 +119,7 @@ void hash_table_v1_add_entry(struct hash_table_v1 *hash_table,
 	list_entry->key = key;
 	list_entry->value = value;
 	SLIST_INSERT_HEAD(list_head, list_entry, pointers);
-	unlock_lock_v1();
+	unlock_lock_v1(&hash_table->lock);
 }
 
 uint32_t hash_table_v1_get_value(struct hash_table_v1 *hash_table,
@@ -133,7 +134,7 @@ uint32_t hash_table_v1_get_value(struct hash_table_v1 *hash_table,
 
 void hash_table_v1_destroy(struct hash_table_v1 *hash_table)
 {
-	destroy_lock_v1();
+	destroy_lock_v1(&hash_table->lock);
 	for (size_t i = 0; i < HASH_TABLE_CAPACITY; ++i) {
 		struct hash_table_entry *entry = &hash_table->entries[i];
 		struct list_head *list_head = &entry->list_head;
